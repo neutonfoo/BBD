77cfaf4dd68a4434c5877783f3a37e98bead6f2c
neutonfoo <neuton.foo@gmail.com>
2018-04-14 11:21:52 -0700

    Major instrument update

--- instLoader.js
+++ instLoader.js
@@ -31,98 +31,43 @@
 //==============================================================================
 	samplesInsts.piano = { name: 'Piano', baseUrl: instsFolder + 'piano/', volume: -10 }
 	samplesInsts.piano.notes = {
-		'E4' : 'E4.mp3',
-		'F4' : 'F4.mp3',
-		'G4' : 'G4.mp3',
-		'A4' : 'A4.mp3',
-		'B4' : 'B4.mp3',
-
-		'C5' : 'C5.mp3',
-		'D5' : 'D5.mp3',
-		'E5' : 'E5.mp3',
-		'F5' : 'F5.mp3',
-		'G5' : 'G5.mp3',
-		'A5' : 'A5.mp3'
+		'C3' : 'C2.mp3',
+		'C4' : 'C3.mp3',
+		'C5' : 'C4.mp3',
+		'C6' : 'C5.mp3',
+		'C7' : 'C6.mp3',
+		'C8' : 'C7.mp3'
 	}
-
-	// samplesInsts.piano = { name: 'Piano', baseUrl: 'piano/', volume: 0 }
-	// samplesInsts.piano.notes = {
-	// 	'A0' : 'A0.mp3',
-	// 	'C1' : 'C1.mp3',
-	// 	'D#1' : 'Ds1.mp3',
-	// 	'F#1' : 'Fs1.mp3',
-	// 	'A1' : 'A1.mp3',
-	// 	'C2' : 'C2.mp3',
-	// 	'D#2' : 'Ds2.mp3',
-	// 	'F#2' : 'Fs2.mp3',
-	// 	'A2' : 'A2.mp3',
-	// 	'C3' : 'C3.mp3',
-	// 	'D#3' : 'Ds3.mp3',
-	// 	'F#3' : 'Fs3.mp3',
-	// 	'A3' : 'A3.mp3',
-	// 	'C4' : 'C4.mp3',
-	// 	'D#4' : 'Ds4.mp3',
-	// 	'F#4' : 'Fs4.mp3',
-	// 	'A4' : 'A4.mp3',
-	// 	'C5' : 'C5.mp3',
-	// 	'D#5' : 'Ds5.mp3',
-	// 	'F#5' : 'Fs5.mp3',
-	// 	'A5' : 'A5.mp3',
-	// 	'C6' : 'C6.mp3',
-	// 	'D#6' : 'Ds6.mp3',
-	// 	'F#6' : 'Fs6.mp3',
-	// 	'A6' : 'A6.mp3',
-	// 	'C7' : 'C7.mp3',
-	// 	'D#7' : 'Ds7.mp3',
-	// 	'F#7' : 'Fs7.mp3',
-	// 	'A7' : 'A7.mp3',
-	// 	'C8' : 'C8.mp3'
-	// }
 
 //==============================================================================
 // Guitar
 //==============================================================================
-	samplesInsts.guitar = { name: 'Guitar', baseUrl: instsFolder + 'guitar/', volume: -15 }
+	samplesInsts.guitar = { name: 'Electric Guitar', baseUrl: instsFolder + 'electricGuitar/', volume: -15 }
 	samplesInsts.guitar.notes = {
-		'C3' : 'C3.mp3',
-		'D3' : 'D3.mp3',
-		'E3' : 'E3.mp3',
-		'F3' : 'F3.mp3',
-		'G3' : 'G3.mp3',
-		'A3' : 'A3.mp3',
-		'B3' : 'B3.mp3',
-
-		'C4' : 'C4.mp3',
-		'D4' : 'D4.mp3',
-		'E4' : 'E4.mp3',
-		'F4' : 'F4.mp3',
-		'G4' : 'G4.mp3',
-		'A4' : 'A4.mp3',
-		'B4' : 'B4.mp3',
-
-		'C5' : 'C5.mp3',
-		'D5' : 'D5.mp3',
-		'E5' : 'E5.mp3',
-		'F5' : 'F5.mp3',
-		'G5' : 'G5.mp3',
-		'A5' : 'A5.mp3',
-		'B5' : 'B5.mp3'
+		'C3' : 'C2.mp3',
+		'C4' : 'C3.mp3',
+		'C5' : 'C4.mp3',
+		'C6' : 'C5.mp3'
 	}
+
 
 //==============================================================================
 // Strings
 //==============================================================================
 	samplesInsts.strings = { name: 'Violin', baseUrl: instsFolder + 'violin/', volume: -8 }
 	samplesInsts.strings.notes = {
-		'C5': 'C5.mp3',
-		'F5': 'F5.mp3',
-		'A5': 'A5.mp3'
+		'C3' : 'C2.mp3',
+		'C4' : 'C3.mp3',
+		'C5' : 'C4.mp3',
+		'C6' : 'C5.mp3',
+		'C7' : 'C6.mp3',
+		'C8' : 'C7.mp3'
 	}
 
 //==============================================================================
 // Bass
 //==============================================================================
-	samplesInsts.bass = { name: 'Double Bass', baseUrl: instsFolder + 'doublebass/', volume: -8 }
+	samplesInsts.bass = { name: 'Double Bass', baseUrl: instsFolder + 'doubleBass/', volume: -3 }
 	samplesInsts.bass.notes = {
 		'A1' : 'A1.mp3',
 		'C2' : 'C2.mp3',
@@ -138,7 +83,7 @@
 //==============================================================================
 // Drums
 //==============================================================================
-	samplesInsts.drums = { name: 'Drums', baseUrl: './drums/fixed/', volume: -15 }
+	samplesInsts.drums = { name: 'Drums', baseUrl: instsFolder + 'drums/', volume: -15 }
 	samplesInsts.drums.notes = {
 		'G#2' : 'Gs2.mp3',
 		'B1' : 'B1.mp3',
@@ -152,14 +97,14 @@
 //==============================================================================
 // Brass
 //==============================================================================
-		samplesInsts.brass = { name: 'Trumpet', baseUrl: instsFolder + 'brass/', volume: -8 }
-		samplesInsts.brass.notes = {
-			'A4' : 'A4.mp3',
-			'B4' : 'B4.mp3',
-			'C5' : 'C5.mp3',
-			'F4' : 'F4.mp3',
-			'F5' : 'F5.mp3'
-		}
+	samplesInsts.brass = { name: 'Trumpet', baseUrl: instsFolder + 'trumpet/', volume: -8 }
+	samplesInsts.brass.notes = {
+		'C3' : 'C2.mp3',
+		'C4' : 'C3.mp3',
+		'C5' : 'C4.mp3',
+		'C6' : 'C5.mp3',
+		'C7' : 'C6.mp3'
+	}
 
 //==============================================================================
 // Load Buffers
