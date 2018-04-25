d8a6b80fe85b20b7fc43614884a23ae786b63fc7
neutonfoo <neuton.foo@gmail.com>
2018-04-25 09:42:56 -0700

    Added Flutes and Updated Violins

--- instLoader.js
+++ instLoader.js
@@ -100,7 +100,6 @@
 		'B6' : 'B6.mp3',
 
 		'C7' : 'C7.mp3'
-
 	}
 	masterInsts.piano.insts.push(pianoGrandPiano)
 
@@ -139,7 +138,7 @@
 	masterInsts.bass = { name: 'Bass' }
 	masterInsts.bass.insts = []
 
-	// Bass / Double Bass
+	// Bass / Fingerstyle Bass
 	var bassDoubleBass = { instCode: 'bassFingerstyleBass', name: 'Fingerstyle Bass', default: true, volume: 0 }
 	bassDoubleBass.notes = {
 		'E1' : 'E1.mp3',
@@ -205,7 +204,7 @@
 	}
 	masterInsts.bass.insts.push(bassDoubleBass)
 
-	// Bass / Double Bass
+	// Bass / Muted Bass
 	var bassDoubleBass = { instCode: 'bassMutedBass', name: 'Muted Bass', volume: 0 }
 	bassDoubleBass.notes = {
 		'E1' : 'E1.mp3',
@@ -263,25 +262,142 @@
 //==============================================================================
 // iF - Strings
 //==============================================================================
+
 	masterInsts.strings = { name: 'Strings' }
 	masterInsts.strings.insts = []
 
-	var stringsViolin = { instCode: 'stringsViolin', name: 'Violin', default: true, volume: 0 }
+// Strings / Violin
+	var stringsViolin = { instCode: 'stringsViolin', name: 'Violin', default: true, volume: -7 }
 	stringsViolin.notes = {
-		'C3' : 'C2.mp3',
-		'C4' : 'C3.mp3',
-		'C5' : 'C4.mp3',
-		'C6' : 'C5.mp3',
-		'C7' : 'C6.mp3',
-		'C8' : 'C7.mp3'
+		'C3' : 'C3.mp3',
+		'C#3' : 'Cs3.mp3',
+		'D3' : 'D3.mp3',
+		'D#3' : 'Ds3.mp3',
+		'E3' : 'E3.mp3',
+		'F3' : 'F3.mp3',
+		'F#3' : 'Fs3.mp3',
+		'G3' : 'G3.mp3',
+		'G#3' : 'Gs3.mp3',
+		'A3' : 'A3.mp3',
+		'A#3' : 'As3.mp3',
+		'B3' : 'B3.mp3',
+
+		'C4' : 'C4.mp3',
+		'C#4' : 'Cs4.mp3',
+		'D4' : 'D4.mp3',
+		'D#4' : 'Ds4.mp3',
+		'E4' : 'E4.mp3',
+		'F4' : 'F4.mp3',
+		'F#4' : 'Fs4.mp3',
+		'G4' : 'G4.mp3',
+		'G#4' : 'Gs4.mp3',
+		'A4' : 'A4.mp3',
+		'A#4' : 'As4.mp3',
+		'B4' : 'B4.mp3',
+
+		'C5' : 'C5.mp3',
+		'C#5' : 'Cs5.mp3',
+		'D5' : 'D5.mp3',
+		'D#5' : 'Ds5.mp3',
+		'E5' : 'E5.mp3',
+		'F5' : 'F5.mp3',
+		'F#5' : 'Fs5.mp3',
+		'G5' : 'G5.mp3',
+		'G#5' : 'Gs5.mp3',
+		'A5' : 'A5.mp3',
+		'A#5' : 'As5.mp3',
+		'B5' : 'B5.mp3',
+
+		'C6' : 'C6.mp3',
+		'C#6' : 'Cs6.mp3',
+		'D6' : 'D6.mp3',
+		'D#6' : 'Ds6.mp3',
+		'E6' : 'E6.mp3',
+		'F6' : 'F6.mp3',
+		'F#6' : 'Fs6.mp3',
+		'G6' : 'G6.mp3',
+		'G#6' : 'Gs6.mp3',
+		'A6' : 'A6.mp3',
+		'A#6' : 'As6.mp3',
+		'B6' : 'B6.mp3',
+
+		'C7' : 'C7.mp3'
 	}
 	masterInsts.strings.insts.push(stringsViolin);
+
+//==============================================================================
+// Reed
+//==============================================================================
+	masterInsts.reed = { name: 'Reed' }
+	masterInsts.reed.insts = []
+
+// Reed / Flute
+
+	var reedFlute= { instCode: 'reedFlute',  name: 'Flute', default: true, volume: 0 }
+	reedFlute.notes = {
+		'C3' : 'C3.mp3',
+		'C#3' : 'Cs3.mp3',
+		'D3' : 'D3.mp3',
+		'D#3' : 'Ds3.mp3',
+		'E3' : 'E3.mp3',
+		'F3' : 'F3.mp3',
+		'F#3' : 'Fs3.mp3',
+		'G3' : 'G3.mp3',
+		'G#3' : 'Gs3.mp3',
+		'A3' : 'A3.mp3',
+		'A#3' : 'As3.mp3',
+		'B3' : 'B3.mp3',
+
+		'C4' : 'C4.mp3',
+		'C#4' : 'Cs4.mp3',
+		'D4' : 'D4.mp3',
+		'D#4' : 'Ds4.mp3',
+		'E4' : 'E4.mp3',
+		'F4' : 'F4.mp3',
+		'F#4' : 'Fs4.mp3',
+		'G4' : 'G4.mp3',
+		'G#4' : 'Gs4.mp3',
+		'A4' : 'A4.mp3',
+		'A#4' : 'As4.mp3',
+		'B4' : 'B4.mp3',
+
+		'C5' : 'C5.mp3',
+		'C#5' : 'Cs5.mp3',
+		'D5' : 'D5.mp3',
+		'D#5' : 'Ds5.mp3',
+		'E5' : 'E5.mp3',
+		'F5' : 'F5.mp3',
+		'F#5' : 'Fs5.mp3',
+		'G5' : 'G5.mp3',
+		'G#5' : 'Gs5.mp3',
+		'A5' : 'A5.mp3',
+		'A#5' : 'As5.mp3',
+		'B5' : 'B5.mp3',
+
+		'C6' : 'C6.mp3',
+		'C#6' : 'Cs6.mp3',
+		'D6' : 'D6.mp3',
+		'D#6' : 'Ds6.mp3',
+		'E6' : 'E6.mp3',
+		'F6' : 'F6.mp3',
+		'F#6' : 'Fs6.mp3',
+		'G6' : 'G6.mp3',
+		'G#6' : 'Gs6.mp3',
+		'A6' : 'A6.mp3',
+		'A#6' : 'As6.mp3',
+		'B6' : 'B6.mp3',
+
+		'C7' : 'C7.mp3'
+	}
+	masterInsts.reed.insts.push(reedFlute)
 
 //==============================================================================
 // Brass
 //==============================================================================
 	masterInsts.brass = { name: 'Brass' }
 	masterInsts.brass.insts = []
+
+// Brass / Trumpet
 
 	var brassTrumpet= { instCode: 'brassTrumpet',  name: 'Trumpet', default: true, volume: 0 }
 	brassTrumpet.notes = {
@@ -299,7 +415,7 @@
 	masterInsts.synth = { name: 'Synth'};
 	masterInsts.synth.insts = []
 
-	// Piano / Grand Piano
+// Synth / Dream Dancer
 	var synthDreamDancer = { instCode: 'synthDreamDancer', name: 'Dream Dancer', default: true, volume: 0 }
 	synthDreamDancer.notes = {
 		'C3' : 'C2.mp3',
